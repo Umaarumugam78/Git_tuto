b238f9647f7f7dfe9cb4fcb76a4a4e8e07c6aee9
Umaarumugam78 <umaarumugam78@gmail.com>
2017-02-25 18:55:17 -0800

    Add files via upload

---
+++ Def_fun.py
@@ -0,0 +1,15 @@
+def age(name):
+        print("age of",name)
+        
+
+def add(a,b):
+        return (a*b)
+
+      
+        sum=add(a*b)
+
+def kidsgames(name,*games):
+            print(name)
+            print(games)
+
+input()
---
+++ test1.py
@@ -0,0 +1,8 @@
+A=int(input("Enter your 1st value"))
+B=int(input("Enter your 2nd value"))
+C=int(input("Enter your 3rd value"))
+
+print ("Maxiumum Value")
+print(max(A,B,C))
+
+input("enter to exit")
